2cf51e01b7b3b3ac6b530b028da3c1a1d507aa88
ezram <ezram@W10-D-EZRAM>
2019-01-05 02:42:07 -0600

    Showing errors even on prod for now.

--- Avery16828Generator.PL/Startup.cs
+++ Avery16828Generator.PL/Startup.cs
@@ -31,14 +31,15 @@ namespace Avery16828Generator.PL
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-            }
+           app.UseDeveloperExceptionPage();
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+            //else
+            //{
+            //    app.UseExceptionHandler("/Error");
+            //}
 
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
