17064a9739668eaa1ede0760faaa3246b2d8024a
ezram <ezram@W10-D-EZRAM>
2019-01-06 08:24:13 -0600

    Added some logging in startup.

--- Avery16828Generator.PL/Startup.cs
+++ Avery16828Generator.PL/Startup.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -31,6 +33,10 @@ namespace Avery16828Generator.PL
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            var linesToWrite = new List<string>();
+            linesToWrite.Add($"Environment name: {env.EnvironmentName}");
+            linesToWrite.Add($"IsDevelopment: {env.IsDevelopment()}");
+            File.WriteAllLines("c:\\startuplog.txt", linesToWrite);
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
