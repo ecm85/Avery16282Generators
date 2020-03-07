ddcd5e53e9ba9e45b23639bb668dd14f7cef3c98
Ezra <ezramcnichols@gmail.com>
2020-03-07 17:44:57 -0600

    Added Legacy,BuriedSecrets,TheNewAge,ShatteredDreams,TheAncients,IntoTheWild expansions. Added back in Spark and Crystal cards (whoops!).

--- Avery16282Generator/AeonsEnd/Expansion.cs
+++ Avery16282Generator/AeonsEnd/Expansion.cs
@@ -9,7 +9,13 @@ namespace Avery16282Generator.AeonsEnd
         TheNameless,
         TheOuterDark,
         TheVoid,
-        TheDepths
+        TheDepths,
+        Legacy,
+        BuriedSecrets,
+        TheNewAge,
+        ShatteredDreams,
+        TheAncients,
+        IntoTheWild
     }
 
     public static class ExpansionExtensions
@@ -30,6 +36,18 @@ namespace Avery16282Generator.AeonsEnd
                     return "The Void";
                 case Expansion.TheDepths:
                     return "The Depths";
+                case Expansion.Legacy:
+                    return "Legacy";
+                case Expansion.BuriedSecrets:
+                    return "Buried Secrets";
+                case Expansion.TheNewAge:
+                    return "The New Age";
+                case Expansion.ShatteredDreams:
+                    return "Shattered Dreams";
+                case Expansion.TheAncients:
+                    return "The Ancients";
+                case Expansion.IntoTheWild:
+                    return "Into The Wild";
                 default:
                     throw new InvalidOperationException();
             }
@@ -51,6 +69,18 @@ namespace Avery16282Generator.AeonsEnd
                     return "V";
                 case Expansion.TheDepths:
                     return "D";
+                case Expansion.Legacy:
+                    return "L";
+                case Expansion.BuriedSecrets:
+                    return "BS";
+                case Expansion.TheNewAge:
+                    return "NA";
+                case Expansion.ShatteredDreams:
+                    return "SD";
+                case Expansion.TheAncients:
+                    return "A";
+                case Expansion.IntoTheWild:
+                    return "IW";
                 default:
                     throw new InvalidOperationException();
             }
